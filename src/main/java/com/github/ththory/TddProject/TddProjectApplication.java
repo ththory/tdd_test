281ac7c0a36f99f54fe5a8b0a58ac3f3b2985b84
ththory <ththory@github.io>
2024-08-09 13:23:33 +0900

    Init

---
+++ src/main/java/com/github/ththory/TddProject/TddProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.ththory.TddProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddProjectApplication.class, args);
+	}
+
+}
